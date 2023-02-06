32efd829d4e8127bf21f697a55ecaf492d3470e2
Niel Mueller <niel.mueller@edu.tbz.ch>
2023-02-06 13:50:09 +0100

    finish get data for mache_move

--- src/server.js
+++ src/server.js
@@ -9,6 +9,50 @@ var bodyParser = require('body-parser');
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// functions
+
+// check if key exist and update time
+function check_key(Key){
+    // check if key is in database
+    const check_key = db.prepare("SELECT * FROM Key WHERE Key = @Key");
+    const check = check_key.get({Key});
+    if(check != undefined){
+        // check if key is not out time
+        if(new Date() / 1 - check["time"] > 10000){
+            // delete Key
+            const delete_key = db.prepare("DELETE FROM Key WHERE Key = @Key");
+            delete_key.run({Key});
+            return false;
+        }
+        else{
+            // set new time for key
+            const update_key = this.db.prepare("UPDATE Key SET time = @Time WHERE Key = @Key");
+            var Time = new Date() / 1;
+            return true;
+        }
+
+    }
+    else{
+        return false;
+    }
+}
+
+// returns the User_ID from the Key
+function get_player(Key){
+    const get_player = db.prepare("SELECT User_FK FROM Key WHERE Key = @Key");
+    return get_player.run({Key});
+}
+
+// check if spiel exist and if user has rights to access
+function spielexist(spiel_id, Player){
+const spielexist = db.prepare("SELECT Player_2, aktueller_player FROM Games WHERE (Player_2 = @Player OR Player_1 = @Player) AND Games_ID = @spiel_id");
+const check_spiel = spielexist.run({Player, spiel_id});
+if(check_spiel =! undefined){
+    return true;
+}
+return false;
+}
+
 
 const genAPIKey = () => {
     //create a base-36 string that contains 30 chars in a-z,0-9
@@ -26,8 +70,8 @@ app.post('/login', async function (req, res) {
             let api_key = genAPIKey();
             var time = new Date() / 1;
             const user_ID = check["User_ID"];
-            const insertKEY = db.prepare("INSERT INTO Key (time, User_FK) VALUES (@time, @user_ID)");
-            insertKEY.run({time, user_ID })
+            const insertKEY = db.prepare("INSERT INTO Key (time, User_FK, Key) VALUES (@time, @user_ID, @api_key");
+            insertKEY.run({time, user_ID,  api_key})
             res.send(api_key);
         }
         else{
@@ -43,7 +87,10 @@ app.post('/login', async function (req, res) {
 app.post('/mache_move', async function (req, res) {
 try{
     let {KEY, spiel_id, anfangx, anfangy, endex, endey} = req.body;
-    const get_type = db.prepare("SELECT Type FROM Figuren WHERE Player = ");
+    if(!(await check_key(key))) res.send("ungültiger KEY");
+    var Player = get_player(key);
+    if(!spielexist(spiel_id, Player)) res.send("ungültiges Spiel");
+    const get_type = db.prepare("SELECT Type FROM Figuren WHERE Player = @Player AND Games_ID = @spiel_id");
     get_type.run({anfangx, anfangy})
     var farbe = true;
     var spielfigur = 2;
@@ -66,6 +113,7 @@ try{
             case 6:
               day = "Saturday";
           }
+
         }
     else if(farbe == false){
         switch (spielfigur) {
@@ -89,11 +137,14 @@ try{
     }
 }
 catch(error){
-    res.send()
+    res.send("Error")
 }
 })
 
 function getposition(x,y,spiel_id){
+
+}
+function eat(x,y,spiel_id){
 
 }
 
